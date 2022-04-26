01517625154586fb10e40d8d5ad46b0df7fbeb9e
John William Davis <jwdavisdeveloper@gmail.com>
2022-04-26 11:17:38 -0700

    Add custom favicon

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,11 @@ import Title from "../components/Title";
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>a styled-components starter</title>
+        <meta name='description' content='A Starter for styled-components' />
+        <link rel='icon' href='/wave.ico' />
+      </Head>
       <Title>This is my styled component!</Title>
     </div>
   );
